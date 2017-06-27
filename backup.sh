d8eaba403dbb3c440bf17ef2852fee2f1b3e4fbd
Tshaba Phomolo Benedict <benedicttshaba@gmail.com>
2017-06-27 12:39:49 +0200

    Create backup.sh
    Todo: Error handling

---
+++ backup.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+####################################
+#
+# Backup Script - Tshaba P.B
+# Usage: ./backup.sh username "/home /etc /root /opt"
+#
+####################################
+
+# What to backup. 
+backup_files=$2
+
+USER=$1
+
+# Where to backup to.
+dest="/media/$USER/BackupDrive"
+
+# Create archive filename.
+day=$(date +%A-%F-%X)
+archive_file="backup-$USER@$HOSTNAME-$day.tgz"
+snapshot="snapshot.snar"
+
+# Print start status message.
+echo "Tshaba P.B's System Backup Utility"
+date
+echo
+echo "Backing up $backup_files to $dest/$archive_file"
+echo
+
+# Backup the files using tar.
+tar czf $dest/$archive_file -g $dest/$snapshot $backup_files
+
+# Print end status message.
+echo
+echo "Backup finished"
+date
+
+# Long listing of files in $dest to check file sizes.
+ls -lh $dest
+
+# m h dom mon dow   command
+# 0 0 * * * bash /usr/local/bin/backup.sh
