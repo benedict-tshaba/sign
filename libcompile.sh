c49209f22500ac8208be368838ba42d998488123
Tshaba Phomolo Benedict <benedicttshaba@gmail.com>
2017-08-14 07:21:50 +0200

    Add libcompile.sh

---
+++ libcompile.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+libtype="$1"
+program="$2"
+outfile="$3"
+
+usage()
+{
+	printf "Usage: $0 static/[dynamic] program.c(.cpp) output.a[.so]\n"
+	exit 0
+}
+
+if [ -z "$libtype" ]
+then
+	usage
+fi
+
+if [ -z "$program" ]
+then
+	usage
+fi
+
+if [ ! -e "$program" ]
+then
+	echo "No such file of directory: $program"
+	exit 1
+fi
+
+if [ "$libtype" == "static" ]
+then
+	gcc -Wall -Werror -c $program
+	ar -cvq lib$outfile *.o
+	ar -t lib$outfile
+	rm *.o
+fi
+
+if [ "$libtype" == "dynamic" ]
+then
+	gcc -Wall -Werror -fPIC -c $program
+	gcc -shared -Wl,-soname,lib$outfile.1 -o lib$outfile.1.0 *.o
+	rm *.o
+fi
